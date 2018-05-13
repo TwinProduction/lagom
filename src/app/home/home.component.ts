eb8271d58a0299a39d548f72da19198ef0490c45
TwinProduction <christian-ct@hotmail.com>
2018-05-13 19:41:08 -0400

    Fixed issue with not all images being displayed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 
-const FLICKR_REQUEST_URL = "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=580e51fb0ef83cdbf92411eb93ecf005&user_id=141118288%40N08&per_page=20&format=json&nojsoncallback=1";
-
 
 @Component({
   selector: 'app-home',
@@ -12,36 +10,50 @@ const FLICKR_REQUEST_URL = "https://api.flickr.com/services/rest/?method=flickr.
 })
 export class HomeComponent implements OnInit {
 
-  photos = [];
+  photoRows:object[] = [];
+  currentPage:number = 1;
 
 
   constructor(private http: HttpClient) {
-    this.http.get(FLICKR_REQUEST_URL).subscribe(data => {
+    this.http.get(this.getApiUrl(1)).subscribe(data => {
       console.log(data);
-      var photos = data['photos']['photo'];
+      let photos = data['photos']['photo'];
+      let currentRow = [];
       for (let i in photos) {
         let photo = photos[i];
+        console.log("photo ", i, photo);
         let photoUrl = this.imageUrlBuilder(photo['farm'], photo['server'], photo['id'], photo['secret']);
-        //console.log(photoUrl);
-        this.photos.push({url: photoUrl, name: photo['title']});
+        if (currentRow.length < 2 && +i !== photos.length - 1) {
+          currentRow.push({url: photoUrl, name: photo['title']});
+        } else {
+          currentRow.push({url: photoUrl, name: photo['title']});
+          console.log("pushing row with " + currentRow.length + " elements");
+          this.photoRows.push(currentRow);
+          currentRow = [];
+        }
+        //this.photos.push({url: photoUrl, name: photo['title']});
       }
     });
   }
 
-
   ngOnInit() {
+  }
+
+
+  getApiUrl(page) {
+    return "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos" +
+      "&api_key=580e51fb0ef83cdbf92411eb93ecf005" +
+      "&user_id=141118288%40N08" +
+      "&per_page=6" +
+      "&page="+ page +
+      "&format=json" +
+      "&nojsoncallback=1"
   }
 
 
 
   imageUrlBuilder(farmId, serverId, id, secret) {
     return "https://farm"+farmId+".staticflickr.com/"+serverId+"/"+id+"_"+secret+".jpg";
-  }
-
-
-  renderPhotos() {
-    console.log("rendering photos");
-    return this.photos;
   }
 
 }
