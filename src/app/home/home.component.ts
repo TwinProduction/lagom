5af6f2c3dea7fd69f5274dbc172030f055e0489d
TwinProduction <christian-ct@hotmail.com>
2018-05-13 17:54:41 -0400

    Home improvements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 
-const FLICKR_REQUEST_URL = "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=35386aee3ec9c8398c501b2552e789d9&user_id=141118288%40N08&per_page=20&format=json&nojsoncallback=1";
+const FLICKR_REQUEST_URL = "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=580e51fb0ef83cdbf92411eb93ecf005&user_id=141118288%40N08&per_page=20&format=json&nojsoncallback=1";
 
 
 @Component({
@@ -23,7 +23,7 @@ export class HomeComponent implements OnInit {
         let photo = photos[i];
         let photoUrl = this.imageUrlBuilder(photo['farm'], photo['server'], photo['id'], photo['secret']);
         //console.log(photoUrl);
-        this.photos.push({url: photoUrl, name: photo['title'], num: i});
+        this.photos.push({url: photoUrl, name: photo['title']});
       }
     });
   }
