bf95c4169145c9057c098b4f2d15e820c1d0ff60
TwinProduction <christian-ct@hotmail.com>
2018-05-13 20:32:54 -0400

    Added page control and beautified UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,31 +12,34 @@ export class HomeComponent implements OnInit {
 
   photoRows:object[] = [];
   currentPage:number = 1;
-
+  maxPage:number = 1;
 
   constructor(private http: HttpClient) {
-    this.http.get(this.getApiUrl(1)).subscribe(data => {
-      console.log(data);
+    this.fetchPhotos();
+  }
+
+  ngOnInit() {
+  }
+
+
+  fetchPhotos() {
+    this.http.get(this.getApiUrl(this.currentPage)).subscribe(data => {
+      this.photoRows = [];
       let photos = data['photos']['photo'];
+      this.maxPage = data['photos']['pages'];
       let currentRow = [];
       for (let i in photos) {
         let photo = photos[i];
-        console.log("photo ", i, photo);
         let photoUrl = this.imageUrlBuilder(photo['farm'], photo['server'], photo['id'], photo['secret']);
         if (currentRow.length < 2 && +i !== photos.length - 1) {
           currentRow.push({url: photoUrl, name: photo['title']});
         } else {
           currentRow.push({url: photoUrl, name: photo['title']});
-          console.log("pushing row with " + currentRow.length + " elements");
           this.photoRows.push(currentRow);
           currentRow = [];
         }
-        //this.photos.push({url: photoUrl, name: photo['title']});
       }
     });
-  }
-
-  ngOnInit() {
   }
 
 
@@ -44,7 +47,7 @@ export class HomeComponent implements OnInit {
     return "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos" +
       "&api_key=580e51fb0ef83cdbf92411eb93ecf005" +
       "&user_id=141118288%40N08" +
-      "&per_page=6" +
+      "&per_page=20" +
       "&page="+ page +
       "&format=json" +
       "&nojsoncallback=1"
@@ -54,6 +57,18 @@ export class HomeComponent implements OnInit {
 
   imageUrlBuilder(farmId, serverId, id, secret) {
     return "https://farm"+farmId+".staticflickr.com/"+serverId+"/"+id+"_"+secret+".jpg";
+  }
+
+
+  nextPage() {
+    this.currentPage++;
+    this.fetchPhotos();
+  }
+
+
+  prevPage() {
+    this.currentPage--;
+    this.fetchPhotos();
   }
 
 }
