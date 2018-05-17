8899d29a4017713310201db32973d5e5bc6c2160
TwinProduction <christian-ct@hotmail.com>
2018-05-17 15:18:33 -0400

    Minor update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import {RouterModule} from "@angular/router";
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
