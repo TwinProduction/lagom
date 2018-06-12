6c8891e0084cd1f059750acd2146091e4b2e4214
TwinProduction <christian-ct@hotmail.com>
2018-06-12 16:22:00 -0400

    Reordered routes import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {HomeComponent} from "./home/home.component";
 import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 
 const routes: Routes = [
